3b8ec00d16f0c11ddae9adffec421222bf9dde2c
veyselyildiz65 <vyildiz1@sheffield.ac.uk>
2024-05-16 15:21:37 +0100

    code optimization

--- Run_Optimisation.py
+++ Run_Optimisation.py
@@ -9,43 +9,79 @@
 ############################################################################
 """
 """ 
-Main File to Run for optimisation
+Main File to Run for Optimization
+
+Parameters for Differential Evolution (DE):
+x(1), typet:  Turbine type (1= Kaplan, 2= Francis, 3 = Pelton turbine)
+
+x(2), conf: Turbine configuration (1= Single, 2= Dual, 3 = Triple, ..nth Operation)
+ 
+x(3), D: Penstock diameter,
+
+x(4) Od1: First turbine design docharge,
+
+x(5) Od2: Second turbine design docharge,
+
+x(6) Od3: Third turbine design docharge,
+...
+x(n) Odn: nth turbine design docharge,
+
 """
 
 # Import  the modules to be used from Library
-import numpy as np
-import pandas as pd
-import math 
-import matplotlib.pyplot as plt
-import statistics
 from scipy.optimize import  differential_evolution
-
-# Import global parameters including site characteristics and streamflow records
-#import HP
+import numpy as np
 
 # Import  the all the functions defined
-#from model_functions import *
-#from opt_energy_functions import *
-from opt_operation_type import opt_config
+from opt_energy_functions import Opt_energy_single, Opt_energy_OP
 
 
-### Parameter range for Differential Evolution (DE)
-## turbine type, turbine configuration, D, QL, QS
+def generate_bounds(numturbine):
+    """
+    Generate the bounds dynamically based on the number of turbines.
+    First two is turbine type and number, third one is for D and rest is for turbines design discharge
+    """
+    base_bounds = [(0.51, 3.49), (0.51, 3.49), (1, 5)]
+    turbine_bounds = [(0.5, 20)] * numturbine
+    return base_bounds + turbine_bounds
 
-# round (0.51-1.49) = 1, Kaplan turbine, 
-# round (1.50-2.49) = 2, Francis turbine, 
-# round (2.50-3.49) = 3, Pelton turbine, 
 
-#Turbine tpype: 1 = Kaplan turbine,  2 = Francis turbine,  3 = Pelton turbine 
-#Turbine configuration: 1 = Single,  2 = Dual,  3 = Triple Operation 
+def opt_config(x):
+    """
+    x, Parameters: Array of design variables including typet, conf, D, and Od values.
+    Dynamically handle the X_in array based on the value of numturbine.
+    
+    Returns: The objective function value for the given configuration.
+    """
+    typet = round(x[0]) # Turbine type
+    conf = round(x[1])  # Turbine configuration (single, dual, triple, etc.)
+    X_in = np.array(x[2:2 + numturbine + 1])# Slicing input array for diameter and turbine design discharges
+    
+    if conf == 1: # 1 turbine
+       return  Opt_energy_single (typet, 1, X_in)
 
-##Structure of DE optimisation
+    else: #conf == 2: more than 1 turbine
+      return  Opt_energy_OP (typet, conf, X_in)
 
-bounds = [(0.51,3.49), (0.51,3.49), (1, 5), (1, 20), (1, 20)]
 
-#result = differential_evolution(opt_config, bounds)
+# Set the number of turbines for optimization
+numturbine = 2  # Example: optimization up to two turbine configurations
+bounds = generate_bounds(numturbine)
 
-result = differential_evolution(opt_config, bounds, maxiter=100, 
-        popsize=10, tol=0.001, mutation=(0.5, 1), recombination=0.7, init='latinhypercube')
+
+# Run the differential evolution optimization
+result = differential_evolution(
+    opt_config, 
+    bounds, 
+    maxiter=100, 
+    popsize=10, 
+    tol=0.001, 
+    mutation=(0.5, 1), 
+    recombination=0.7, 
+    init='latinhypercube'
+)
+
+print(result)
 
 #result = differential_evolution(opt_config, bounds, updating='deferred', workers=-1)
+#result = differential_evolution(opt_config, bounds)
--- Run_Simulation.py
+++ Run_Simulation.py
@@ -4,30 +4,43 @@
 #                     Written by Veysel Yildiz                             #
 #                      vyildiz1@sheffield.ac.uk                            #
 #                   The University of Sheffield                            #
-#                           June  2024                                     #
+#                           June 2024                                      #
 ############################################################################
 """
-""" 
-Main File to Run for Simulation
+""" Main File to Run for simulation
+
+Parameters:
+x(1), typet:  Turbine type (1= Kaplan, 2= Francis, 3 = Pelton turbine)
+
+x(2), conf: Turbine configuration (1= Single, 2= Dual, 3 = Triple, ..nth Operation)
+ 
+x(3), D: Penstock diameter,
+
+x(4) Od1: First turbine design docharge,
+x(5) Od2: Second turbine design docharge,
+...
+x(n) Odn: nth turbine design docharge,
+
+Return :
+       AAE : Annual average energy
+       NPV : Net Present Value in million USD
+       BC  : Benefot to Cost Ratio
 """
 
 # Import  the modules to be used from Library
 import numpy as np
-import pandas as pd
-import math 
-import matplotlib.pyplot as plt
-import statistics
-
-# Import global parameters including site characteristics and streamflow records
-import HP
 
 # Import  the all the functions defined
-from model_functions import *
-from sim_energy_functions import *
-from sim_operation_type import sim_config
+from sim_energy_functions import Sim_energy_single, Sim_energy_OP
 
+def sim_config( typet, conf, X): 
+    if conf == 1: # 1 turbine
+        [AAE, NPV, BC] = Sim_energy_single ( typet, conf, X);
+    else: # 2 or more turbine
+        [AAE,  NPV, BC] = Sim_energy_OP (typet, conf, X); 
+    return AAE,  NPV, BC
 
-# Run the model
+# Setup the model
 typet =  2   # turbine type: 1 = Kaplan, 2 = Francis, 3= Pelton
 conf = 2  # turbine config: 1 = single, 2 = dual, 3 = triple
 D = 2 # as m
@@ -37,6 +50,5 @@ Q2 = 10# as m3/s, design discharge of second turbine
 X =  np.array([D, Q1, Q2])
 
 AAE,  NPV, BC = sim_config (typet, conf, X);
- # P: Daily power, AAE: Annual average energy, OF: Objective Function
 
-display(AAE)
+print(AAE,  NPV, BC)
